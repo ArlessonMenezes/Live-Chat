5a7f1a8ffe7984f658667bfc3057097bd31f4291
Arlesson Menezes <arlesson_10@hotmail.com>
2021-09-12 11:37:21 -0300

    Live Chat

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+
+const io = require('socket.io')(http, {
+    cors: {
+        origin: "http://localhost:3000/",
+        methods: ["GET", "POST"],
+      }
+});
+
+//Criando evento 
+io.on('connection', socket => {
+
+    socket.on("disconnect", () => {
+        console.log('X Desconectou: ' + socket.id)
+    });
+
+
+    socket.on("msg", data => {
+        io.emit("showmsg", data);
+        console.log(data)
+    })
+
+});
+
+
+app.set('view engine', 'ejs');
+
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+http.listen(3000, () => console.log('App rodando'));
